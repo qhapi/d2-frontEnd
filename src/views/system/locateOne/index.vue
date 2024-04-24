a0a6aa610427d52a5f2645bb42c5dc450755081a
qhapi <qiguiyue@sjtu.edu.cn>
2024-04-24 17:13:38 +0800

    commit

--- src/views/system/locateOne/index.vue
+++ src/views/system/locateOne/index.vue
@@ -1,22 +1,24 @@
 <template>
   <d2-container>
-    <el-form>
-      <el-form-item>
+    <el-row>
+      <el-col :span="11">
         <h1>原始合约</h1>
         <d2-highlight :code="inputText" ></d2-highlight>
-      </el-form-item>
-      <el-form-item>
+      </el-col>
+      <el-col :span="1"><div class="grid-content"></div></el-col>
+      <el-col :span="11">
         <h1>修改后合约</h1>
         <d2-highlight :code="outputText" ></d2-highlight>
-      </el-form-item>
-      <el-form-item>
+      </el-col>
+    </el-row>
+    <el-divider></el-divider>
+    <el-row>
         <template>
           <el-radio @change="chooseMethod('1')" v-model="radio" label="1">方案一</el-radio>
           <el-radio @change="chooseMethod('2')" v-model="radio" label="2">方案二</el-radio>
           <el-button>导出修改</el-button>
         </template>
-      </el-form-item>
-    </el-form>
+    </el-row>
   </d2-container>
 </template>
 
